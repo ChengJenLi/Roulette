8598a0b57f24f1d6d0517c4d1c67e9837045ac4c
Cheng Jen Li <neo@cpshs.org.tw>
2022-03-11 22:39:47 +0800

    更新 pxt.json, main.blocks, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+def 編號關燈(燈號: number):
+    global LX, LY
+    LX = 燈號 % 5
+    LY = 燈號 / 5
+    led.unplot(LX, LY)
+
+def on_button_pressed_a():
+    global 亂數, PA
+    亂數 = randint(0, 15)
+    PA = not (PA)
+    basic.clear_screen()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global PA
+    basic.clear_screen()
+    PA = False
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def 編號點燈(燈號2: number):
+    global LX, LY
+    LX = 燈號2 % 5
+    LY = 燈號2 / 5
+    led.plot(LX, LY)
+PB = False
+PA = False
+亂數 = 0
+LY = 0
+LX = 0
+list2 = [0, 1, 2, 3, 4, 9, 14, 19, 24, 23, 22, 21, 20, 15, 10, 5]
+
+def on_forever():
+    global PB
+    if not (PA):
+        for index in range(16):
+            編號點燈(list2[index])
+            basic.pause(100)
+            編號關燈(list2[index])
+        PB = True
+    else:
+        if PB:
+            index2 = 0
+            while index2 <= 亂數:
+                PB = False
+                編號點燈(list2[index2])
+                basic.pause(100)
+                編號關燈(list2[index2])
+                index2 += 1
+            編號點燈(list2[亂數])
+            basic.pause(1000)
+            編號關燈(list2[亂數])
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -5,16 +5,24 @@ function 編號關燈 (燈號: number) {
 }
 input.onButtonPressed(Button.A, function () {
     亂數 = randint(0, 15)
+    PA = !(PA)
+    basic.clearScreen()
 })
-function 編號點燈 (燈號: number) {
-    LX = 燈號 % 5
-    LY = 燈號 / 5
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    PA = false
+})
+function 編號點燈 (燈號2: number) {
+    LX = 燈號2 % 5
+    LY = 燈號2 / 5
     led.plot(LX, LY)
 }
+let PB = false
+let PA = false
 let 亂數 = 0
 let LY = 0
 let LX = 0
-let list = [
+let list2 = [
 0,
 1,
 2,
@@ -33,9 +41,25 @@ let list = [
 5
 ]
 basic.forever(function () {
-    for (let index = 0; index <= 15; index++) {
-        編號點燈(list[index])
-        basic.pause(100)
-        編號關燈(list[index])
+    let index2: number;
+if (!(PA)) {
+        for (let index = 0; index <= 15; index++) {
+            編號點燈(list2[index])
+            basic.pause(100)
+            編號關燈(list2[index])
+        }
+        PB = true
+    } else if (PB) {
+        index2 = 0
+        while (index2 <= 亂數) {
+            PB = false
+            編號點燈(list2[index2])
+            basic.pause(100)
+            編號關燈(list2[index2])
+            index2 += 1
+        }
+        編號點燈(list2[亂數])
+        basic.pause(1000)
+        編號關燈(list2[亂數])
     }
 })
